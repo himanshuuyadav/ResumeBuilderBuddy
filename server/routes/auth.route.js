b84488ecd1ed3b544d5a9537a1c6432cf0920f73
himanshu <its.himanshuyadav11@gmail.com>
2025-06-29 23:07:45 +0530

    add test route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,6 +3,10 @@ const { googleSignIn } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
+router.get('/test', (req, res) => {
+  res.send('✅ Backend working!');
+});
+
 //google sign-in
 router.post('/google-sign-in', googleSignIn);
 
