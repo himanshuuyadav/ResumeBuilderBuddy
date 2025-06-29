88ed52f16fb2258813e98b7eab9dc8ba88aae019
himanshu <its.himanshuyadav11@gmail.com>
2025-06-29 23:29:21 +0530

    frontend

---
+++ client/src/api.js
@@ -0,0 +1,4 @@
+//export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL = "https://resume-builder-zkad.onrender.com/api"
+//export const BASE_URL = "https://resume-builder-mern-one.vercel.app/api"
+export const BASE_URL = "https://resumebuilderbuddy-1.onrender.com/api";
