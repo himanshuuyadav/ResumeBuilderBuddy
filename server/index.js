a0a6ce6f887dd55ab630a200d92949b735c0d0a6
himanshu <its.himanshuyadav11@gmail.com>
2025-06-29 23:15:52 +0530

    Cors Setup

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ const app = express();
 mongoDB();
 
 // CORS Setup (must come early)
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // Parse JSON bodies
 app.use(express.json());
